686b4b322302147ba53f757e691373f0c7b9d84c
AnnaKurilova <41905405+AnnaKurilova@users.noreply.github.com>
2020-06-23 17:47:23 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 // DEBOUNCING HELPER FUNCTION
 
-const debounce = (func, delay = 1000) => { // with a 1 sec delay default value
+const debounce = (func, delay = 1000) => { 
   
   // function wrapper, a shield how often the func should be invoked
   let timeoutId;
@@ -15,19 +15,3 @@ const debounce = (func, delay = 1000) => { // with a 1 sec delay default value
 };
 
 
-
-
-// we call our function and search for the results only in 1 second after the user stops to write  
-
-// let timeoutId; // is a ID parameter of setTimeout for stopping the executing setTimeout function
-// const onInput = event => {
-//     if (timeoutId) {      // to look up if timeoutId is defined
-//         clearTimeout(timeoutId); // if user stops to write in input we stop the pending timer // stop existing one and start a new one
-//     }
-   
-//    timeoutId = setTimeout(() => {
-//         fetchData(event.target.value); // get the fetch data from the server in 1 second
-//     }, 1000)    
-// };
-
-// input.addEventListener('input', onInput);
